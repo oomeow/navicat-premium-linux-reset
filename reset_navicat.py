300bffe823e6d180d22957201998461dffbc8b7f
JokingAboutLife <2502993659@qq.com>
2023-11-08 12:24:22 +0800

    Use Pylint to format the code

--- reset_navicat.py
+++ reset_navicat.py
@@ -1,3 +1,6 @@
+"""
+Filename: reset_navicat.py
+"""
 import os
 import subprocess
 import datetime
@@ -39,18 +42,18 @@ def get_json_data(file_path):
     从json文件中读取数据
     '''
     dicts = {}
-    with open(file_path,'r',encoding='utf8') as f:
+    with open(file_path, 'r', encoding='utf8') as f:
         json_data = json.load(f)
         dicts = json_data
     return dicts
 
 
-def write_json_data(file_path, dict):
+def write_json_data(file_path, data):
     '''
     写入数据到json文件中
     '''
-    with open(file_path,'w') as r:
-        json.dump(dict, r, indent=4)
+    with open(file_path, 'w', encoding="utf8") as r:
+        json.dump(data, r, indent=4)
 
 
 def check_network_connection_status(check_timeout_minutes=60, check_time_interval_second=2):
@@ -61,13 +64,13 @@ def check_network_connection_status(check_timeout_minutes=60, check_time_interva
     network_check_count = check_timeout_minutes * 60 / check_time_interval_second
     while not status and network_check_count > 0:
         network_check_count -= 1
-        result = subprocess.run('ping baidu.com -c 1', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if not result.returncode:
+        network_check = subprocess.run('ping baidu.com -c 1', shell=True,
+                                       stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+        if not network_check.returncode:
             status = True
             break
-        else:
-            log.debug('网络未连接，%ss 后将再次进行检查', check_time_interval_second)
-            sleep(check_time_interval_second)
+        log.debug('网络未连接，%ss 后将再次进行检查', check_time_interval_second)
+        sleep(check_time_interval_second)
     if status:
         log.info('网络已连接')
     else:
@@ -84,26 +87,33 @@ def check_ntp_service_status(check_timeout_minutes=10):
     while not status and ntp_service_check_count > 0:
         # 检查 NTP 时间同步服务是否启动
         ntp_service_check_count -= 1
-        result = subprocess.run('timedatectl status | grep NTP | cut -d ":" -f 2', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        ntp_service_msg = result.stdout
+        ntp_check = subprocess.run('timedatectl status | grep NTP | cut -d ":" -f 2',
+                                   shell=True, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   text=True, check=False)
+        ntp_service_msg = ntp_check.stdout
         if "inactive" in ntp_service_msg:
             log.info('NTP 时间同步服务未启动，即将重新开启 NTP 时间同步服务')
             log.debug('5s 后将再次检查 NTP 时间同步服务是否启动')
             # sudo 交互式输入命令设置 ntp
-            # p1 = subprocess.Popen('sudo -S timedatectl set-ntp false', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # p1 = subprocess.Popen('sudo -S timedatectl set-ntp false', shell=True,
+            #           stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             # outs, errs = p1.communicate(bytes(AUTH_PASSWORD, 'utf-8'), timeout=10)
             # p1.wait()
             # log.debug(str(outs, 'utf-8'))
             # log.debug(str(errs, 'utf-8'))
-            subprocess.run('timedatectl set-ntp false', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run('timedatectl set-ntp false', shell=True,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
             sleep(1)
             # sudo 交互式输入命令设置 ntp
-            # p2 = subprocess.Popen('sudo -S timedatectl set-ntp true', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # p2 = subprocess.Popen('sudo -S timedatectl set-ntp true', shell=True,
+            #           stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             # outs, errs = p2.communicate(bytes(AUTH_PASSWORD, 'utf-8'), timeout=10)
             # p2.wait()
             # log.debug(str(outs, 'utf-8'))
             # log.debug(str(errs, 'utf-8'))
-            subprocess.run('timedatectl set-ntp true', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run('timedatectl set-ntp true', shell=True,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
             sleep(4)
         else:
             status = True
@@ -123,8 +133,10 @@ def check_time_sync_status(check_timeout_minutes=60, check_time_interval_second=
     time_sync_check_count = check_timeout_minutes * 60 / 5
     while not status and time_sync_check_count > 0:
         time_sync_check_count -= 1
-        result = subprocess.run('timedatectl status | grep synchronized | cut -d ":" -f 2', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        synced = result.stdout.strip()
+        time_check = subprocess.run('timedatectl status | grep synchronized | cut -d ":" -f 2',
+                                    shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                    text=True, check=False)
+        synced = time_check.stdout.strip()
         if 'yes' in synced:
             status = True
             break
@@ -168,15 +180,14 @@ def need_to_reset_navicat():
             trial_period = datetime.datetime.strptime(trial_period_str, DATE_FORMAT_PATTERN)
             # 距试用期剩余天数
             count_days = (trial_period - now_date).days
-            if count_days > 0 and count_days <= 14:
+            if 0 < count_days <= 14:
                 log.info('试用期至 %s, 下次重置将在 %d 天后进行', trial_period_str, count_days)
                 return False
-            elif count_days == 0:
+            if count_days == 0:
                 log.info('距试用期 %s 不到一天时间，即将重置 navicat 试用期', trial_period_str)
                 return True
-            else:
-                log.info('距离试用过期过长或过短，即将重置 navicat 试用期')
-                return True
+            log.info('距离试用过期过长或过短，即将重置 navicat 试用期')
+            return True
         except JSONDecodeError as e:
             log.error('解析json文件错误：%s', e)
             log.info('脚本重置信息的 json 文件无法解析，即将重置 navicat 试用期，重新生成json信息文件')
@@ -205,7 +216,7 @@ def update_navicat_reset_json_data(reseted):
         'trial_period': trial_period_str
     }
     write_json_data(RESET_JSON_INFO_FILE, reset_json)
-            
+
 
 def reset_navicat():
     '''
@@ -217,10 +228,11 @@ def reset_navicat():
         json_data.pop('B966DBD409B87EF577C9BBF3363E9614', None)
         write_json_data(NAVICAT_PREFERENCES_JSON_PATH, json_data)
         update_navicat_reset_json_data(True)
-        result = subprocess.run(DCONF_RESET_CMD, shell=True, capture_output=True, text=True)
-        return_code = result.returncode
-        if return_code:
-            error_msg = result.stderr
+        reset_need = subprocess.run(DCONF_RESET_CMD, shell=True, capture_output=True,
+                                    text=True, check=False)
+        reset_need_code = reset_need.returncode
+        if reset_need_code:
+            error_msg = reset_need.stderr
             log.error('dconf 重置 navicat 出现错误：%s', error_msg)
         else:
             log.info('dconf 重置 navicat 成功')
@@ -229,36 +241,36 @@ def reset_navicat():
         log.info('无需重置 navicat 试用期')
 
 
+# 全局变量
+DATE_FORMAT_PATTERN = '%Y-%m-%d %H:%M:%S'
+USER_HOME_DIR = os.environ['HOME']
+# sudo 需要输入的密码
+# AUTH_PASSWORD = '123456'
+BASE_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+NAVICAT_PREFERENCES_JSON_PATH = USER_HOME_DIR + '/.config/navicat/Premium/preferences.json'
+RESET_JSON_INFO_FILE = BASE_FILE_DIR + '/reset_navicat.json'
+LOGGER_PATH = BASE_FILE_DIR + '/logs/reset_navicat.log'
+DCONF_RESET_CMD = 'dconf reset -f /com/premiumsoft/navicat-premium/'
+now_date = datetime.datetime.now()
+log = logger_config(LOGGER_PATH, console_print=False)
+
+
 if __name__ == '__main__':
-    # 常量
-    DATE_FORMAT_PATTERN = '%Y-%m-%d %H:%M:%S'
-    USER_HOME_DIR = os.environ['HOME']
-    # sudo 需要输入的密码
-    # AUTH_PASSWORD = '123456'
-    BASE_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-    NAVICAT_PREFERENCES_JSON_PATH = USER_HOME_DIR + '/.config/navicat/Premium/preferences.json'
-    RESET_JSON_INFO_FILE = BASE_FILE_DIR + '/reset_navicat.json'
-    LOGGER_PATH = BASE_FILE_DIR + '/logs/reset_navicat.log'
-    DCONF_RESET_CMD = 'dconf reset -f /com/premiumsoft/navicat-premium/'
-    
-    # 变量
-    now_date = datetime.datetime.now()
-    log = logger_config(LOGGER_PATH, console_print=False)
-    
     log.info('-----------------------------'.center(50, '-'))
     log.info('检查重置 navicat 试用期'.center(50))
     log.info('-----------------------------'.center(50, '-'))
     # dconf 是清除 navicat 的主要命令
-    result = subprocess.run('command -v dconf > /dev/null 2>&1', shell=True)
+    result = subprocess.run('command -v dconf > /dev/null 2>&1', shell=True, check=False)
     return_code = result.returncode
     if return_code:
         log.error('命令 dconf 不存在，请先安装 dconf')
     else:
-        sync_status = check_network_clock_sync()
-        if sync_status:
+        SYNC_STATUS = check_network_clock_sync()
+        if SYNC_STATUS:
             now_date = datetime.datetime.now()
             log.info('时间已从网络中同步：%s', now_date.strftime(DATE_FORMAT_PATTERN))
         else:
             now_date = datetime.datetime.now()
-            log.info('时间同步失败，使用本机系统时间：%s，该时间直接影响到 navicat 的试用期', now_date.strftime(DATE_FORMAT_PATTERN))
+            log.info('时间同步失败，使用本机系统时间：%s，该时间直接影响到 navicat 的试用期',
+                     now_date.strftime(DATE_FORMAT_PATTERN))
         reset_navicat()
