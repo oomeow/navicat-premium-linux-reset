555b9d74bf640c538ee8063211c34040303a3e6f
oomeow <oomeow@outlook.com>
2024-05-09 13:40:07 +0800

    fix: KDE does not execute script after user login

--- install.sh
+++ install.sh
@@ -4,15 +4,27 @@ CURRENT_SCRIPT_DIR=$(
 	cd "$(dirname "$0")" || exit 1
 	pwd
 )
+CURRENT_DESKTOP=$(echo "${XDG_CURRENT_DESKTOP}")
 CURRENT_SCRIPT_DIR_FIXED=$(echo "${CURRENT_SCRIPT_DIR}" | sed "s|$HOME|\\\$HOME|")
 PROFILE_PATH="${HOME}/.profile"
+SCRIPT_PATH=${CURRENT_SCRIPT_DIR}/reset-navicat.sh
+# Even if the command to execute the script is added in the .profile file,
+# 	the script is not executed in KDE of archLinux, so use the autostart.
+KDE_AUTOSTART_DIR=$HOME/.config/autostart/
 
 install() {
+	if [[ "$CURRENT_DESKTOP" == "KDE" ]]; then
+		sed "s|script_path|${SCRIPT_PATH}|g" ./template.desktop >reset_navicat.sh.desktop
+		mv ./reset_navicat.sh.desktop ${KDE_AUTOSTART_DIR}
+	fi
 	sed -i '/\/reset-navicat.sh/d' "${PROFILE_PATH}" >/dev/null 2>&1
-	echo "bash \"${CURRENT_SCRIPT_DIR_FIXED}/reset-navicat.sh\"" >>"${PROFILE_PATH}"
+	echo "bash \"${SCRIPT_PATH}\"" >>"${PROFILE_PATH}"
 }
 
 uninstall() {
+	if [[ "$CURRENT_DESKTOP" == "KDE" ]]; then
+		rm ${KDE_AUTOSTART_DIR}/reset_navicat.sh.desktop
+	fi
 	sed -i '/\/reset-navicat.sh/d' "${PROFILE_PATH}" >/dev/null 2>&1
 }
 
--- reset-navicat.sh
+++ reset-navicat.sh
@@ -5,45 +5,45 @@ PYTHON_BIN_PATH=""
 
 # 错误信息显示
 message() {
-    TITLE="Cannot start reset Navicat"
-    if [ -n "$(command -v zenity)" ]; then
-        zenity --error --title="$TITLE" --text="$1" --no-wrap
-    elif [ -n "$(command -v kdialog)" ]; then
-        kdialog --error "$1" --title "$TITLE"
-    elif [ -n "$(command -v notify-send)" ]; then
-        notify-send "ERROR: $TITLE" "$1"
-    elif [ -n "$(command -v xmessage)" ]; then
-        xmessage -center "ERROR: $TITLE: $1"
-    else
-        printf "ERROR: %s\n%s\n" "$TITLE" "$1"
-    fi
+	TITLE="Cannot start reset Navicat"
+	if [ -n "$(command -v zenity)" ]; then
+		zenity --error --title="$TITLE" --text="$1" --no-wrap
+	elif [ -n "$(command -v kdialog)" ]; then
+		kdialog --error "$1" --title "$TITLE"
+	elif [ -n "$(command -v notify-send)" ]; then
+		notify-send "ERROR: $TITLE" "$1"
+	elif [ -n "$(command -v xmessage)" ]; then
+		xmessage -center "ERROR: $TITLE: $1"
+	else
+		printf "ERROR: %s\n%s\n" "$TITLE" "$1"
+	fi
 }
 
 # 检查 python 是否安装, 获取 python 命令路径
 if [ -n "$(command -v python)" ] || [ -n "$(command -v python3)" ]; then
-    for tool in python python3; do
-        test -n "$(command -v $tool)" && PYTHON_BIN_PATH="$(command -V $tool | awk '{print $3}')"
-    done
-    if [ -z "$PYTHON_BIN_PATH" ]; then
-        message "no python or python3 command, please install python."
-        exit 1
-    fi
+	for tool in python python3; do
+		test -n "$(command -v $tool)" && PYTHON_BIN_PATH="$(command -V $tool | awk '{print $3}')"
+	done
+	if [ -z "$PYTHON_BIN_PATH" ]; then
+		message "no python or python3 command, please install python."
+		exit 1
+	fi
 fi
 
 # 检查需要的工具
 if [ -z "$(command -v timedatectl)" ] || [ -z "$(command -v dconf)" ]; then
-    TOOLS_MSG="Required tools are missing:\n"
-    for tool in timedatectl dconf; do
-        test -z "$(command -v $tool)" && TOOLS_MSG="$TOOLS_MSG $tool\n"
-    done
-    message "$TOOLS_MSG"
-    exit 1
+	TOOLS_MSG="Required tools are missing:\n"
+	for tool in timedatectl dconf; do
+		test -z "$(command -v $tool)" && TOOLS_MSG="$TOOLS_MSG $tool\n"
+	done
+	message "$TOOLS_MSG"
+	exit 1
 fi
 
 # 获取当前文件目录
 CURRENT_SCRIPT_DIR=$(
-    cd "$(dirname "$0")" || exit 1
-    pwd
+	cd "$(dirname "$0")" || exit 1
+	pwd
 )
 
 # 后台执行 python 脚本
